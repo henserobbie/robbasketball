1fde8a03d09a51a1e4df4ed79798d3b439a8db9c
henserobbie <henserobbie@gmail.com>
2024-02-08 19:26:53 -0500

    Removed balancing from compileData

--- src/compileData.py
+++ src/compileData.py
@@ -22,11 +22,5 @@ for stat in stats:
     march[stat+'2'] = march.apply(lambda row: \
                     teamStats.loc[(teamStats['Season'] == row['Season']) & (teamStats['TeamID'] == row['TeamID2']), stat].values[0], axis=1)
 
-# duplicate and switch teams 1 and 2
-switched = march.copy()
-switched[[stat+'1' for stat in stats] + [stat+'2' for stat in stats] + ['TeamID1', 'Score1', 'TeamID2', 'Score2']] = \
-    march[[stat+'2' for stat in stats] + [stat+'1' for stat in stats] + ['TeamID2', 'Score2', 'TeamID1', 'Score1']]
-marchFinal = pd.concat([march, switched], ignore_index=True)
-
 # save march games
-marchFinal.to_csv(f'{basePath}/data/processed/marchGames.csv', index=False)
+march.to_csv(f'{basePath}/data/processed/marchGames.csv', index=False)
